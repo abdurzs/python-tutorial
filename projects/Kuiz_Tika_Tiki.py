60922af4c38158145666d2bcf6db4755c8d9a6dc
abdurzs <abdurrahmanzul435@gmail.com>
2024-03-09 16:05:41 +0800

    markah

--- projects/Kuiz_Tika_Tiki.py
+++ projects/Kuiz_Tika_Tiki.py
@@ -6,27 +6,34 @@ if playing.lower() != "ya":
     quit()
 
 print("Ok! Jom Tika Tiki :)")
+markah = 0
 
 jawab = input("Perkataan apa yang dieja salah dalam kamus? ")
 if jawab.lower() == "salah":
     print("Tepat sekali!")
+    markah += 1
 else:
     print("Cuba lagi!")
 
 jawab = input("Bom apakah yang tidak boleh meletup? ")
 if jawab.lower() == "bomba":
     print("Tepat sekali!")
+    markah += 1
 else:
     print("Cuba lagi!")
 
 jawab = input("Dalam banyak-banyak bunga, bunga apa warna hitam? ")
 if jawab.lower() == "bunga tayar":
     print("Tepat sekali!")
+    markah += 1
 else:
     print("Cuba lagi!")
 
 jawab = input("Katak diterbalikkan jadi katak. Kopi di terbalikkan jadi? ")
 if jawab.lower() == "tumpah":
     print("Tepat sekali!")
+    markah += 1
 else:
     print("Cuba lagi!")
+
+print("Mantap, anda dapat jawab " + str(markah) + " soalan dengan betul! ")
