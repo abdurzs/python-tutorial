ccee7a76e46e280fea98e57412376b3671d30419
abdurzs <abdurrahmanzul435@gmail.com>
2024-03-10 21:58:43 +0800

    input setup

--- projects/tikatiki_nombor.py
+++ projects/tikatiki_nombor.py
@@ -1,3 +1,16 @@
 import random
 
-random.randrange(0,11)
+top_of_range = input("Tuliskan satu nombor: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Sila tulis satu nombor yang melebihi 0.")
+        quit()
+else:
+    print("Sila tulis nombor sahaja.")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+print(random_number)
